f77c3a954aeb0952ae09ac6e16e21a3e6630a831
ricmozz8 <cupcakethief2311@gmail.com>
2024-12-24 04:42:43 -0400

    Update font family in tailwind.config.js and app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,8 +10,9 @@
         <link rel="icon" type="image/x-icon" href="{{ asset('favicon.svg') }}" />
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@12..96,200..800&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
         @routes
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Bricolage Grotesque', ...defaultTheme.fontFamily.sans],
             },
         },
     },
