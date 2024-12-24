ebd604400a991bbbfeb8d5ea6660fa407c38e396
ricmozz8 <cupcakethief2311@gmail.com>
2024-12-24 04:53:03 -0400

    Update font family and add custom color in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,20 @@ export default {
             fontFamily: {
                 sans: ['Bricolage Grotesque', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                classydog: {
+                    '50': '#e6e0f2',
+                    '100': '#cdc1e5',
+                    '200': '#b3a1d8',
+                    '300': '#9981cb',
+                    '400': '#7f61be',
+                    '500': '#53108c',
+                    '600': '#4f0f8c',
+                    '700': '#3b0b66',
+                    '800': '#270740',
+                    '900': '#13031a',
+                },
+            },
         },
     },
 
